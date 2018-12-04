514b072d9cbeb938569f898afa5b0a9b03d4d044
Audi <audi-mac@audis-mac.local>
2018-12-03 20:27:12 -0600

    Initial commit Added schema Added TableViewControllers for both instructor and ta Added Cells for both instructor and ta

---
+++ OfficeHours/InstructorCell.swift
@@ -0,0 +1,24 @@
+//
+//  InstructorCell.swift
+//  OfficeHours
+//
+//  Created by Audi on 12/3/18.
+//  Copyright © 2018 Audi. All rights reserved.
+//
+
+import UIKit
+
+class InstructorCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ OfficeHours/TACell.swift
@@ -0,0 +1,24 @@
+//
+//  TACell.swift
+//  OfficeHours
+//
+//  Created by Audi on 12/3/18.
+//  Copyright © 2018 Audi. All rights reserved.
+//
+
+import UIKit
+
+class TACell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
